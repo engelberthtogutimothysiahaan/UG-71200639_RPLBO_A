ebc174c6ca2ce8b5e456852448ae1075b0c48aef
engelberthtogutimothysiahaan <79085738+engelberthtogutimothysiahaan@users.noreply.github.com>
2023-01-04 09:58:55 +0700

    Add files via upload

---
+++ TAS-71200639/TokoMonster/src/main/java/com/uas/prakrplbo/Cyclops.java
@@ -0,0 +1,19 @@
+package com.uas.prakrplbo;
+
+public class Cyclops extends Monster{
+
+        public Cyclops(String nama) {
+            super(nama);
+        }
+
+        @Override
+        public String bersuara() {
+            return "Hohohoho";
+        }
+
+        @Override
+        public String bergerak() {
+            return super.getNama()+" Berjalan...";
+        }
+    }
+
